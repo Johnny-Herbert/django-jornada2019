0f66a957411f74789fef680c20cccce981fe6c4e
Johnny Herbert Muniz Nunes <jhmn@ug2c26.windows.cin.ufpe.br>
2019-07-23 12:17:35 -0300

    Criando os posts

---
+++ djangogram/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    auth = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    image = models.ImageField(verbose_name='Imagem', upload_to='posts/')
+    description = models.CharField(max_length=256, verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    modified_at = models.DateTimeField(auto_now=True, verbose_name='Modificado em')
+
+    def __str__(self):
+        return f'Post {self.pk} | Author {self.auth} | Created at {self.created_at}'
+    
+    class Meta:
+        verbose_name = 'Postagem'
+        verbose_name_plural = 'Postagens'
---
+++ djangogram/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/CIN/jhmn/Desktop/django-jornada2019/djangogram/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
